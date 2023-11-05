6865368a3e80fec3c80cc7a7ba28f21203ca9455
itcast <hello@itcast.cn>
2023-11-05 14:54:55 +0800

    initial project

---
+++ src/main/java/com/project/BackendSupplyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSupplyManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSupplyManagementSystemApplication.class, args);
+    }
+
+}
