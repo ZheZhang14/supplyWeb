f362eb72a664fd2742d6d4fac61c83fd1f6db568
zz <zz@zzdeMacBook-Pro.local>
2023-11-06 15:03:36 -0800

    v2 forecast function hasn't been implemented yet

--- js/common.js
+++ js/common.js
@@ -2,9 +2,15 @@ var currentUrl = window.location.href;
 
 // Check if the URL contains a specific string
 if (currentUrl.indexOf('sign') !== -1) {
-    $("#header-placeholder").load("header.html");
+    $("#header-placeholder").load("header.html?v=1.1");
 } else {
-    $("#header-placeholder").load("header2.html");
+    $("#header-placeholder").load("header2.html?v=1.1", function(response, status, xhr) {
+        if (status != "error") {
+            console.log($("#profileBtn"));
+            $("#profileBtn").attr("href", "profile.html?userId=" + localStorage.getItem('userId'));
+            $("#profileBtn").text(localStorage.getItem('username'));
+        }
+    });
 }
 
 // Function to calculate MD5 hash of a password
@@ -16,8 +22,22 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
+function getParamValue(k) {
+  const queryParams = new URLSearchParams(window.location.search);
+  var v = null;
+  if (queryParams.size == 0) {
+    return v;
+  }
+  queryParams.forEach((value, key) => {
+    if (key == k) {
+        v = value;
+    }
+  });
+  return v;
+}
+
 $(document).ready(function() {
-     $(document).on('click', '#logout', function(event) {
+    $(document).on('click', '#logout', function(event) {
         event.preventDefault();
         localStorage.clear();
         window.location.href = 'signin.html';
